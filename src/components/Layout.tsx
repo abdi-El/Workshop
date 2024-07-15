bfbf7ba3a5e37d1a8a8fa0910b7d6ca8a2f5580c
Ab <abdelilah@alfred.com>
2024-07-15 16:58:38 +0200

    updated routes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,81 +1,78 @@
-import { HomeOutlined, ProfileOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
-import React from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
-import "../main.css";
+import { ProfileOutlined } from '@ant-design/icons'
+import { Layout, Menu, Row, theme } from 'antd'
+import React from 'react'
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom'
+import '../main.css'
 
 export const pathConstants = {
-    HOMEPAGE: {
-        key: "/",
-        label: "Homepage",
-        icon: <HomeOutlined />
-      },
     ESTIMATES: {
-        key: "estimates",
-        label: "Preventivi",
-        icon: <ProfileOutlined />
-    }
-  }
+        key: '/estimates',
+        label: 'Preventivi',
+        icon: <ProfileOutlined />,
+    },
+}
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer } = Layout
 
-
-
-let paths = Object.keys(pathConstants).map((label)=>{
+let paths = Object.keys(pathConstants).map((label) => {
     return pathConstants[label as keyof typeof pathConstants]
-    })
-
-
+})
 
 const BaseLayout: React.FC = () => {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken()
+    const location = useLocation();
+    const navigate = useNavigate()
 
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Header
+                style={{
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 1,
+                    width: '100%',
+                    display: 'flex',
+                }}
+            >
+                <Row align={'middle'}>
+                  <Link to="/">
+                    <img
+                        src="/src/assets/logo.png"
+                        alt="logo of the app"
+                        height={'60px'}
+                        style={{marginRight: "10px"}}
+                    />
+                  </Link>
+                </Row>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    items={paths}
+                    style={{ flex: 1 }}
+                    onClick={(item) => {
+                        navigate(item.key)
+                    }}
+                    selectedKeys={[location.pathname]}
+                />
+            </Header>
+            <Content style={{ padding: '10px 10px' }}>
+                <div
+                    style={{
+                        padding: 24,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    <Outlet />
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Workshop Manager ©{new Date().getFullYear()} Created by AB
+            </Footer>
+        </Layout>
+    )
+}
 
-  const navigate = useNavigate()
-
-  return (
-    <Layout style={{minHeight: "100vh",}}>
-      <Header
-        style={{
-          position: 'sticky',
-          top: 0,
-          zIndex: 1,
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={paths}
-          style={{ flex: 1, minWidth: 0 }}
-          onClick={(item)=>{
-            navigate(item.key)
-          }}    
-        />
-      </Header>
-      <Content style={{ padding: '10px 10px' }}>
-        
-        <div
-          style={{
-            padding: 24,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <Outlet/>
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Workshop Manager ©{new Date().getFullYear()} Created by AB
-      </Footer>
-    </Layout>
-  );
-};
-
-export default BaseLayout;
+export default BaseLayout
--- src/router.tsx
+++ src/router.tsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
         element: <BaseLayout />,
         children: [
             {
-                path: pathConstants.HOMEPAGE.key,
+                path: "/",
                 element: <Home />,
             },
             {
