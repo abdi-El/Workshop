e5432c5222be10a408bf4168561df20ebabc09f2
Ab <abdelilah@alfred.com>
2024-07-16 15:37:33 +0200

    added base price to settings

--- src/components/forms/SettingsForm.tsx
+++ src/components/forms/SettingsForm.tsx
@@ -1,17 +1,23 @@
-import { Button, Form, Input } from 'antd'
+import { Button, Form, Input, InputNumber, message } from 'antd'
 import { useShallow } from 'zustand/react/shallow'
 import useGlobalStore from '../../stores/GlobalStore'
+import { Settings } from '../../types/data'
 
 export default function SettingsForm() {
     const [form] = Form.useForm()
     const settings = useGlobalStore(useShallow((state) => state.settings))
     const updateSettings = useGlobalStore((state) => state.updateSettings)
 
+    function onFinish(values: Settings) {
+        updateSettings(values)
+        message.success('Impostazioni aggiornate')
+    }
+
     return (
         <Form
             layout={'horizontal'}
             form={form}
-            onFinish={updateSettings}
+            onFinish={onFinish}
             initialValues={settings}
             name="SettingsForm"
         >
@@ -48,6 +54,19 @@ export default function SettingsForm() {
             >
                 <Input placeholder="Esempio: 0789746532" />
             </Form.Item>
+
+            <Form.Item
+                label="Prezzo base ora"
+                name="default_workforce_price"
+                rules={[
+                    {
+                        required: true,
+                    },
+                ]}
+            >
+                <InputNumber placeholder="20" prefix="€" />
+            </Form.Item>
+
             <Form.Item>
                 <Button type="primary" htmlType="submit">
                     Aggiorna dati
--- src/stores/GlobalStore.ts
+++ src/stores/GlobalStore.ts
@@ -2,17 +2,12 @@ import { create } from 'zustand'
 import { Settings } from '../types/data'
 
 interface GlobalState {
-  settings: Settings
+  settings: Settings | {}
   updateSettings: (newSettings: Partial<Settings>) => void
 }
 
 const useGlobalStore = create<GlobalState>()((set) => ({
-  settings: {
-    workshop_name: "Officina Leporatti",
-    p_iva: "3215464",
-    address: "via ghibellina 20",
-    isDarkTheme: true,
-  },
+  settings: {},
   updateSettings: (newSettings) => {set((state) => ({ settings: {...state.settings, ...newSettings} }))},
 }))
 
