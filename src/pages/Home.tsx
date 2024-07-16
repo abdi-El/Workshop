43f125eb71c454829b02ff5fab19f3441301cb2f
Ab <abdelilah@alfred.com>
2024-07-16 15:06:18 +0200

    added feed back button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
+import { invoke } from '@tauri-apps/api'
+import { Button, message } from 'antd'
+
 export default function HomePage() {
-    return <h1>Homepage</h1>
+    function greet() {
+        invoke('greet', { name: 'LEPORATTI' }).then((result) => {
+            message.success(result as string)
+        })
+    }
+
+    return (
+        <h1>
+            <Button onClick={greet}>CLICK ME</Button>
+        </h1>
+    )
 }
