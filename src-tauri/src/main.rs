0582127969caf10d44923b0c21f3adf826dd1aa9
Ab <abdelilah@alfred.com>
2024-07-18 11:05:50 +0200

    fixed format

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-
 use tauri_plugin_sql::{Migration, MigrationKind};
 
 fn main() {
@@ -14,7 +13,6 @@ fn main() {
             kind: MigrationKind::Up,
         }
     ];
-
 
     tauri::Builder::default()
         .plugin(
