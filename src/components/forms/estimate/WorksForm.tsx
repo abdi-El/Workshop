a55e536bd7fa07cde6f623d6c8be1a1c91ba8c18
Ab <abdelilah@alfred.com>
2024-07-22 00:14:01 +0200

    added notes

--- src/components/forms/estimate/WorksForm.tsx
+++ src/components/forms/estimate/WorksForm.tsx
@@ -4,77 +4,86 @@ import React from 'react'
 
 const WorksForm: React.FC = () => {
     return (
-        <Form.List name="works_done">
-            {(fields, { add, remove }, { errors }) => (
-                <>
-                    {fields.map(({ key, name, ...restField }) => (
-                        <Form.Item
-                            required={false}
-                            key={key}
-                            style={{ marginBottom: 0 }}
-                        >
+        <>
+            <Form.List name="works_done">
+                {(fields, { add, remove }, { errors }) => (
+                    <>
+                        {fields.map(({ key, name, ...restField }) => (
                             <Form.Item
-                                {...restField}
-                                name={[name, 'name']}
-                                rules={[
-                                    {
-                                        required: true,
-                                    },
-                                ]}
-                                noStyle
+                                required={false}
+                                key={key}
+                                style={{ marginBottom: 0 }}
                             >
-                                <Input.TextArea
-                                    placeholder="Titolo lavoro eseguito"
-                                    style={{ width: '40%' }}
+                                <Form.Item
+                                    {...restField}
+                                    name={[name, 'name']}
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                    noStyle
+                                >
+                                    <Input.TextArea
+                                        placeholder="Titolo lavoro eseguito"
+                                        style={{ width: '40%' }}
+                                    />
+                                </Form.Item>
+                                <Form.Item
+                                    {...restField}
+                                    name={[name, 'price']}
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                    noStyle
+                                >
+                                    <InputNumber
+                                        placeholder="Prezzo lavoro eseguito"
+                                        step="0.01"
+                                        prefix="€"
+                                        style={{ width: '40%' }}
+                                    />
+                                </Form.Item>
+                                <MinusCircleOutlined
+                                    style={{ marginLeft: '5px' }}
+                                    onClick={() => remove(name)}
                                 />
                             </Form.Item>
-                            <Form.Item
-                                {...restField}
-                                name={[name, 'price']}
-                                rules={[
-                                    {
-                                        required: true,
-                                    },
-                                ]}
-                                noStyle
+                        ))}
+                        <Form.Item>
+                            <Button
+                                type="dashed"
+                                onClick={() => add()}
+                                style={{ width: '60%' }}
+                                icon={<PlusOutlined />}
                             >
-                                <InputNumber
-                                    placeholder="Prezzo lavoro eseguito"
-                                    step="0.01"
-                                    prefix="€"
-                                    style={{ width: '40%' }}
-                                />
-                            </Form.Item>
-                            <MinusCircleOutlined
-                                style={{ marginLeft: '5px' }}
-                                onClick={() => remove(name)}
-                            />
+                                Aggiungi un lavoro
+                            </Button>
+                            <Button
+                                type="dashed"
+                                onClick={() => {
+                                    add('', 0)
+                                }}
+                                style={{ width: '60%', marginTop: '20px' }}
+                                icon={<PlusOutlined />}
+                            >
+                                Aggiungi lavoro all'inizio
+                            </Button>
+                            <Form.ErrorList errors={errors} />
                         </Form.Item>
-                    ))}
-                    <Form.Item>
-                        <Button
-                            type="dashed"
-                            onClick={() => add()}
-                            style={{ width: '60%' }}
-                            icon={<PlusOutlined />}
-                        >
-                            Aggiungi un lavoro
-                        </Button>
-                        <Button
-                            type="dashed"
-                            onClick={() => {
-                                add('', 0)
-                            }}
-                            style={{ width: '60%', marginTop: '20px' }}
-                            icon={<PlusOutlined />}
-                        >
-                            Aggiungi lavoro all'inizio
-                        </Button>
-                        <Form.ErrorList errors={errors} />
-                    </Form.Item>
-                </>
-            )}
-        </Form.List>
+                    </>
+                )}
+            </Form.List>
+            <Form.Item name="notes">
+                <Input.TextArea
+                    placeholder="Note"
+                    style={{ width: '80%' }}
+                    rows={5}
+                />
+            </Form.Item>
+        </>
     )
 }
 
