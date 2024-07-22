60bab064ba9548645ba682a37692394545ac0b3c
Ab <abdelilah@alfred.com>
2024-07-22 16:13:48 +0200

    updated component name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { PDFViewer } from '@react-pdf/renderer'
-import EstimatePdf from '../components/pdf/Estimate'
+import EstimatePdf from '../components/pdf/EstimatePdf'
 
 export default function HomePage() {
     return (
