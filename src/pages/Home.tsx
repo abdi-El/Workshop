464813b93318ccd2e7a716e68bbbb356827d3536
Ab <abdelilah@alfred.com>
2024-07-23 12:42:29 +0200

    added homepage text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,3 @@
-import { PDFViewer } from '@react-pdf/renderer'
-import EstimatePdf from '../components/pdf/EstimatePdf'
-
 export default function HomePage() {
-    return (
-        <PDFViewer width="100%" height="1000" className="app">
-            <EstimatePdf />
-        </PDFViewer>
-    )
+    return 'Homepage'
 }
